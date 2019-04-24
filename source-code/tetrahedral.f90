904f131fb39f147929029996f095e3602d964174
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-04-24 17:21:36 -0400

    Update tetrahedral.f90

--- source-code/tetrahedral.f90
+++ source-code/tetrahedral.f90
@@ -1,8 +1,7 @@
 ! 0 is for Owen
 
 ! Minimum value is -0.16 (still below 0)
-! 0 is for Owen
-
+! This program is not perfect and there either exists a bug in this code or in my simulation
 
 program data_format
     implicit none
@@ -17,21 +16,21 @@ program data_format
     real :: dist(4), points(12), orders(21800)
     real :: a, b, c, temp
     
-    side = 4
-    nuse = 10
-    pnum = 2180
+    side = 4 ! Side length of box
+    nuse = 10 ! Number of frames to use
+    pnum = 2180 ! Number of particles
     na = 3
     nskip = 0
-    xtcfile = 'traj.xtc' 
-    outfile = 'tetra.dat'
+    xtcfile = 'traj.xtc' ! Input file
+    outfile = 'tetra.dat' ! Output file
     print *, "Beginning formatting"
 
 
     coordnum = 3 * na * pnum
-    allocate(coord(coordnum))
+    allocate(coord(coordnum)) ! Coordinates to be read in
     coord = 0
     total = pnum * nuse
-    allocate(results(total, 16))
+    allocate(results(total, 16)) ! The output, the starting coordinate and the 4 other coordinates, with the final one being the order paramter
     nframe = 0
     numone = 0
     open(unit = 10, file = outfile)
@@ -42,14 +41,16 @@ program data_format
             do while (ret == 1 .and. nframe < (nskip + nuse))
                     call readxtc(uz, natoms, istep, time, gbox, &
                     coord, prec, ret)
-                    do k = 1, size(coord), 9
+					do k = 1, size(coord), 9
+						! Go through the coordinates to find the order parameter for each one
                         points = 0
                         dist = 10
                         c1 = c1 + 1
                         results(c1, 1) = coord(k)
                         results(c1, 2) = coord(k+1)
                         results(c1, 3) = coord(k+2)
-                        do m = 1, size(coord), 9
+						do m = 1, size(coord), 9
+							! Go through all the other coordinates to find the nearest 4
                             if (m == k) cycle
                             dx = abs(results(c1, 1) - coord(m))
                             dy = abs(results(c1, 2) - coord(m + 1))
@@ -57,7 +58,8 @@ program data_format
                             if (dx > 0.5 * side) dx = dx - side
                             if (dy > 0.5 * side) dy = dy - side
                             if (dz > 0.5 * side) dz = dz - side
-                            length = sqrt(dx**2 + dy**2 + dz**2)
+							length = sqrt(dx**2 + dy**2 + dz**2)
+							! Keeping track of the nearest four distances and points
                             if(length < maxval(dist)) then
                                 c2 = (maxloc(dist, DIM=1) - 1) * 3 + 1
                                 dist(maxloc(dist)) = length
@@ -65,12 +67,15 @@ program data_format
                                 points(c2+1) = coord(m+1)
                                 points(c2+2) = coord(m+2)
                             end if
-                        end do
+						end do
+						! Copy the near points over
                         do m = 1, size(points)
                             results(c1, m+3) = points(m)
                         end do
                         sigma = 0
-                        do i = 1, 9, 3
+						do i = 1, 9, 3
+							! This is the calculation of the order paramter, using the law of cosines
+							! Here's a good website for more information http://www.phy.cuhk.edu.hk/opus/comments_2014/lcc_pre.pdf
                             ! I know I am doing needless calculations, I merely do so for readability purposes
                             do j = i + 3, 12, 3
                                 dx = abs(results(c1, 1) - points(i))
@@ -121,9 +126,12 @@ program data_format
     end if
 
     print *, maxval(orders), minval(orders)
-    do i = 1, size(orders) 
+	! These prints are used for debugging
+	do i = 1, size(orders) 
         if (orders(i) < 0) print *, orders(i)
-    end do
+	end do
+	
+	! Write to the file
     do i = 1, total
             do j = 1, 16
                 write(10,"(F10.3)",advance='no') results(i, j)

05f06577746357585eccff00daceaf2f44b8aa6d
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-04-24 18:04:58 -0400

    Update regressnormal.py

--- source-code/different_encoding/regressnormal.py
+++ source-code/different_encoding/regressnormal.py
@@ -1,4 +1,5 @@
 # 0 is for Owen
+# This version is based on the normalization of the coordinates
 import keras 
 from keras import models
 from keras import layers 
@@ -14,6 +15,7 @@ def every_day_im_shuffling(a, b):
 	np.random.set_state(rng_state)
 	np.random.shuffle(b)
 
+# Same input variables as always, size and locations of the information
 COORD = 84
 SIZE = 150248
 BINARY = 84
@@ -24,6 +26,9 @@ labels = np.zeros(shape=SIZE, dtype=np.int64)
 regression = np.zeros(shape=SIZE)
 counter = 0
 zero = 0
+# Reading in the file
+# Note: The restrictions on size in this file and on all regression problems ought to be removed
+# There is no need to have equal 1s and zeros if there it isn't a binary classification problem
 with open("test.dat") as f:
 	for line in f:	
 		if counter >= SIZE:
@@ -49,6 +54,8 @@ with open("test.dat") as f:
 
 print(data[:3])
 print(regression[:3])
+# Because the default 0s don't matter when calculating std and mean (because they are padded values), this is one approach
+# There exists default functions in numpy to do this that would also work
 for i in range(len(data)):
     summ = 0
     numm = 0
@@ -70,6 +77,7 @@ for i in range(len(data)):
 
 print(data[:3])
 print(regression[:3])
+# Shuffle then split
 every_day_im_shuffling(data,regression)
 
 partial_train = data[:int(SIZE/2)]
@@ -80,7 +88,7 @@ test_data = data[int(3*SIZE//4):]
 test_label = regression[int(3*SIZE//4):]
 
 #print(labels[:100])
-
+# As always, hyperparamters subject to optimatization
 model = models.Sequential()
 model.add(layers.Dense(84, activation='relu', input_shape=(84,)))
 model.add(layers.Dense(3, activation='relu'))
@@ -95,6 +103,7 @@ history = model.fit(partial_train, partial_label, shuffle=False, epochs=1000, ba
 results = model.evaluate(test_data, test_label)
 print(results[1])
 pred = model.predict(test_data)
+# Error checking
 #for i in range(len(pred)):
 #	if pred[i] < 0.5 and test_label[i] == 1:
 #		print("False Negative:", regression[i])
@@ -102,7 +111,7 @@ pred = model.predict(test_data)
 
 average_mae_history = history.history['val_mean_absolute_error']
 tmae = history.history['mean_absolute_error']
-
+# Plotting
 a = plt.figure(1)
 plt.plot(range(1, len(average_mae_history) + 1), average_mae_history, label="Validation")
 plt.plot(range(1, len(tmae) + 1), tmae, label="Training")
@@ -133,6 +142,5 @@ b.show()
 
 
 plt.show()
-
 
 

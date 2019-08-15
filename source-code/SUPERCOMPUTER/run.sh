fbb60d88038d313f621175a789468692f0da81fc
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-08-15 15:00:32 -0600

    Update run.sh

--- source-code/SUPERCOMPUTER/run.sh
+++ source-code/SUPERCOMPUTER/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#SBATCH -t 355
-#SBATCH --job-name=TEST 
+#SBATCH --time=10
+#SBATCH --job-name=OWEN_TEST
 #SBATCH --partition dcs
 #SBATCH --nodes 1
 #SBATCH --gres=gpu:4
@@ -10,3 +10,5 @@ path_binary=/gpfs/u/home/MMBS/MMBSwnlc/scratch/conv
 module load xl_r mpich cuda
 
 srun --gres=gpu:4 -n 4 ./bindProcessToGpu.sh python $path_binary/3dconv_dcs.py 
+
+set +x

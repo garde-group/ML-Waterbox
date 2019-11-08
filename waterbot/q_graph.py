1e2597e55224778473350cc9cac85b08ba2795ed
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2019-11-08 17:33:39 -0500

    Update q_graph.py

--- waterbot/q_graph.py
+++ waterbot/q_graph.py
@@ -1,9 +1,11 @@
 '''
 Version Notes
 1.0: Made file, this demonstrates the ML vs existing FORTRAN/actual graph of q 
+1.1: Added heatmap functionality (likely still in process)
 
-Version 1.0
-10/29/2019
+
+Version 1.1
+11/8/2019
 Owen Lockwood
 '''
 
@@ -29,6 +31,7 @@ IN_FILE = "tetra_skip_coord.dat"
 
 all_data = np.zeros(shape=(SIZE, NUM_COORD))
 all_labels = np.zeros(shape=(SIZE))
+
 count = 0
 
 with open(IN_FILE) as f:
@@ -43,6 +46,43 @@ with open(IN_FILE) as f:
 		all_data[count] = l[:NUM_COORD]
 		all_labels[count] = l[NUM_COORD]
 		count += 1
+ss = []
+ss_label = []
+nn_heat = []
+for i in range(4142):
+	if all_data[i][0] < 2.6 and all_data[i][0] > 2.4:
+		ss.append([all_data[i][1], all_data[i][2]])
+		ss_label.append(all_labels[i])
+		nn_heat.append(all_data[i])
+		
+s = len(ss)
+ss = np.asarray(ss)
+#print("SIZE :", s, ss.shape, ss)
+nn_heat = np.asarray(nn_heat)
+ss = np.swapaxes(ss, 0, 1)
+#print("SIZE :", s, ss.shape, ss)
+heat_y = ss[0]
+heat_z = ss[1]
+
+for i in range(len(nn_heat)):
+	x = nn_heat[i][0]
+	y = nn_heat[i][1]
+	z = nn_heat[i][2]
+	j = 3
+	while (j < NUM_COORD):
+		nn_heat[i][j] -= x
+		nn_heat[i][j+1] -= y
+		nn_heat[i][j+2] -= z
+		j += 3
+	nn_heat[i][0] = nn_heat[i][1] = nn_heat[i][2] = 0
+heat_data = np.zeros(shape=(s, 4, 3))
+for i in range(s):
+	j = 3
+	while (j < NUM_COORD):
+		heat_data[i][int(j/3)-1][0] = nn_heat[i][j]
+		heat_data[i][int(j/3)-1][1] = nn_heat[i][j+1]
+		heat_data[i][int(j/3)-1][2] = nn_heat[i][j+2]
+		j += 3
 
 for i in range(SIZE):
 	x = all_data[i][0]
@@ -65,12 +105,12 @@ for i in range(SIZE):
 		td_data[i][int(j/3)-1][2] = all_data[i][j+2]
 		j += 3
 
-for i in range(len(all_labels)):
-	j = math.floor((all_labels[i]+1)/bin)
-	y_act[j] += 1
+#for i in range(len(all_labels)):
+	#j = math.floor((all_labels[i]+1)/bin)
+	#y_act[j] += 1
 
-for i in range(len(y_act)):
-	y_act[i] /= 4142.
+#for i in range(len(y_act)):
+	#y_act[i] /= 4142.
 	#print(x_graph[i], y_act[i])
 
 FORT = np.zeros(shape=(int(2/bin)))
@@ -86,17 +126,23 @@ with open("tetra_skip.dat") as a:
 			except:
 				print(len(l[i]))		
 		#print(final)
-		FORT[math.floor((final[0]+1)/bin)] = final[1]
+		FORT[int(math.floor((final[0]+1)/bin))] = final[1]
 
 model = load_model("tetramodel.h5")
 pre = model.predict(td_data)
 print(pre[0], len(pre))
 for i in range(len(pre)):
 	j = math.floor((pre[i]+1)/bin)
-	ML[j] += 1
+	if (j >= 200):
+		print("HERE", j)
+		j = 199
+	ML[int(j)] += 1
 
 for i in range(len(ML)):
 	ML[i] /= 4142.
+
+
+heat_pre = model.predict(heat_data)
 
 a = plt.figure(1)
 #plt.plot(x_graph, y_act, 'blue', label='Python')
@@ -106,12 +152,26 @@ plt.legend(loc='upper left')
 plt.xlabel('q')
 plt.ylabel('Prob')
 
+# heat map actual
+b = plt.figure(2)
+plt.scatter(heat_y, heat_z, s=100, c=ss_label, cmap="hot", alpha=0.9)
+plt.xlabel("y axis")
+plt.ylabel("z axis")
+plt.title("Actual")
 
+
+# heat map ML
+c = plt.figure(3)
+plt.scatter(heat_y, heat_z, s=100, c=heat_pre, cmap="hot", alpha=0.9)
+plt.xlabel("y axis")
+plt.ylabel("z axis")
+plt.title("ML")
 
 a.show()
+b.show()
+c.show()
 
 plt.show()
-
 
 
 

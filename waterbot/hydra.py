aa7817e9808982ffaaf2011b1082c09c585d604f
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2020-01-28 15:33:28 -0500

    Add files via upload

---
+++ waterbot/hydra.py
@@ -0,0 +1,158 @@
+'''
+Version Notes
+1.0: Creation
+
+
+
+Version 1.0
+1/28/2020
+Owen Lockwood
+'''
+
+
+import numpy as np
+import keras
+import math
+from keras import models
+from keras import layers
+from keras import optimizers
+from keras import backend as K
+import matplotlib.pyplot as plt
+
+
+# Hardcoded defaults
+NUM_COORD = 15
+SIZE = 414200
+IN_FILE = "tetra_coord.dat"
+
+all_data = np.zeros(shape=(SIZE, NUM_COORD))
+all_labels = np.zeros(shape=(SIZE))
+count = 0
+
+with open(IN_FILE) as f:
+	for line in f:
+		if (count >= SIZE):
+			break
+		l = line.split('    ')
+		l = l[1:]
+		l[15] = l[15].strip()		
+		for i in range(len(l)):
+			l[i] = float(l[i])
+		all_data[count] = l[:NUM_COORD]
+		all_labels[count] = l[NUM_COORD]
+		count += 1
+
+#print(all_data[:3])
+#print(all_labels[:3])
+
+'''
+Centering the data, just one approach to try
+'''
+for i in range(SIZE):
+	x = all_data[i][0]
+	y = all_data[i][1]
+	z = all_data[i][2]
+	j = 3
+	while (j < NUM_COORD):
+		all_data[i][j] -= x
+		all_data[i][j+1] -= y
+		all_data[i][j+2] -= z
+		j += 3
+	all_data[i][0] = all_data[i][1] = all_data[i][2] = 0
+
+#print(all_data[:3])
+#print(all_labels[:3])
+
+# To make each array into 2D like Sarupria paper
+cav_labels = np.zeros(shape=(SIZE))
+td_data = np.zeros(shape=(SIZE, 4, 3))
+for i in range(SIZE):
+	j = 3
+	while (j < NUM_COORD):
+		td_data[i][int(j/3)-1][0] = all_data[i][j]
+		td_data[i][int(j/3)-1][1] = all_data[i][j+1]
+		td_data[i][int(j/3)-1][2] = all_data[i][j+2]
+		j += 3
+	dist = 10
+	for k in range(4):
+		dx = abs(td_data[i][k][0])
+		if dx > 2.5:
+			dx -= 5
+		dy = abs(td_data[i][k][1])
+		if dy > 2.5:
+			dy -= 5
+		dz = abs(td_data[i][k][2])
+		if dz > 2.5:
+			dz -= 5
+		temp = math.sqrt(dx**2 + dy**2 + dz**2)
+		if temp < dist:
+			dist = temp
+	cav_labels[i] = dist
+
+print(td_data[0])
+partial_train = td_data[:int(4*SIZE/5)]
+partial_label = all_labels[:int(4*SIZE/5)]
+val_data = td_data[int(4*SIZE/5):int(9*SIZE//10)]
+val_label = all_labels[int(4*SIZE/5):int(9*SIZE//10)]
+test_data = td_data[int(9*SIZE//10):]
+test_label = all_labels[int(9*SIZE//10):]
+partial_label_cav = cav_labels[:int(4*SIZE/5)]
+val_label_cav = cav_labels[int(4*SIZE/5):int(9*SIZE//10)]
+test_label_cav = cav_labels[int(9*SIZE//10):]
+n = 4
+
+cbs = [
+    keras.callbacks.ModelCheckpoint("multi_head.h5", monitor='val_tetra_out_loss', verbose=0, save_best_only=True)
+]
+#np.save("test_data", test_data)
+#np.save("test_label", test_label)
+
+input_shape = (4,3)
+inputs = layers.Input(input_shape)
+x = layers.Dense(32, activation='relu', name='feature1')(inputs)
+x = layers.Dense(64, activation='relu', name='feature2')(x)
+x = layers.Dense(128, activation='relu', name='feature3')(x)
+x = layers.Dense(256, activation='relu', name='feature4')(x)
+x = layers.Dense(512, activation='relu', name='feature5')(x)
+x = layers.Dense(1024, activation='relu', name='feature6')(x)
+x = layers.MaxPooling1D(pool_size=4)(x)
+x = layers.Flatten()(x)
+#x = layers.Dropout(0.3)(x)
+
+tetra = layers.Dense(512, activation='relu', name='tetra1')(x)
+tetra = layers.Dense(256, activation='relu', name='tetra2')(tetra)
+tetra = layers.Dropout(0.3)(tetra)
+tetra = layers.Dense(1, name='tetra_out')(tetra)
+
+cave = layers.Dense(512, activation='relu', name='cave1')(x)
+cave = layers.Dense(256, activation='relu', name='cave2')(cave)
+cave = layers.Dropout(0.3)(cave)
+cave = layers.Dense(1, name='cave_out')(cave)
+
+adam = optimizers.Adam(lr=0.001)
+
+model = models.Model(inputs=inputs, outputs=[cave, tetra])
+
+model.compile(loss={'cave_out': 'mse', 'tetra_out': 'mse'}, metrics={'cave_out': 'mae', 'tetra_out': 'mae'}, optimizer=adam)
+
+history = model.fit(partial_train, [partial_label_cav, partial_label], shuffle=True, epochs=50, \
+batch_size=64, callbacks=cbs, validation_data=(val_data, [val_label_cav, val_label]))
+
+results = model.evaluate(test_data, [test_label_cav, test_label])
+print(results[1])
+
+average_mae_history = history.history['val_mean_absolute_error']
+
+a = plt.figure(1)
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history, 'blue', label='Average MAE')
+plt.legend()
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+
+
+
+a.show()
+
+plt.show()
+
+
---
+++ waterbot/multi_graph.py
@@ -0,0 +1,237 @@
+'''
+Version Notes
+1.0: Made file, this demonstrates the ML vs existing FORTRAN/actual graph of q 
+1.1: Added heatmap functionality (likely still in process)
+1.2: Added Colorbars to heatmaps
+1.3: Properly lined up colorbars
+
+Version 1.3
+12/3/2019
+Owen Lockwood
+'''
+
+import numpy as np
+import keras
+from keras.models import load_model
+from keras import models
+import matplotlib.pyplot as plt
+import math	
+
+
+bin = 0.01
+x_graph = np.zeros(shape=(int(2/bin)))
+
+for i in range(len(x_graph)):
+	x_graph[i] = i*bin-1+0.5*bin
+
+y_act = np.zeros(shape=(int(2/bin)))
+ML = np.zeros(shape=(int(2/bin)))
+ML_cav = np.zeros(shape=(int(0.5/bin)))
+NUM_COORD = 15
+SIZE = 414200
+IN_FILE = "tetra_skip_coord.dat"
+
+all_data = np.zeros(shape=(SIZE, NUM_COORD))
+all_labels = np.zeros(shape=(SIZE))
+
+count = 0
+
+with open(IN_FILE) as f:
+	for line in f:
+		if (count >= SIZE):
+			break
+		l = line.split('    ')
+		l = l[1:]
+		l[15] = l[15].strip()		
+		for i in range(len(l)):
+			l[i] = float(l[i])
+		all_data[count] = l[:NUM_COORD]
+		all_labels[count] = l[NUM_COORD]
+		count += 1
+ss = []
+ss_label = []
+nn_heat = []
+for i in range(4142):
+	if all_data[i][0] < 2.6 and all_data[i][0] > 2.4:
+		ss.append([all_data[i][1], all_data[i][2]])
+		ss_label.append(all_labels[i])
+		nn_heat.append(all_data[i])
+		
+s = len(ss)
+ss = np.asarray(ss)
+#print("SIZE :", s, ss.shape, ss)
+nn_heat = np.asarray(nn_heat)
+ss = np.swapaxes(ss, 0, 1)
+#print("SIZE :", s, ss.shape, ss)
+heat_y = ss[0]
+heat_z = ss[1]
+
+for i in range(len(nn_heat)):
+	x = nn_heat[i][0]
+	y = nn_heat[i][1]
+	z = nn_heat[i][2]
+	j = 3
+	while (j < NUM_COORD):
+		nn_heat[i][j] -= x
+		nn_heat[i][j+1] -= y
+		nn_heat[i][j+2] -= z
+		j += 3
+	nn_heat[i][0] = nn_heat[i][1] = nn_heat[i][2] = 0
+heat_data = np.zeros(shape=(s, 4, 3))
+for i in range(s):
+	j = 3
+	while (j < NUM_COORD):
+		heat_data[i][int(j/3)-1][0] = nn_heat[i][j]
+		heat_data[i][int(j/3)-1][1] = nn_heat[i][j+1]
+		heat_data[i][int(j/3)-1][2] = nn_heat[i][j+2]
+		j += 3
+
+for i in range(SIZE):
+	x = all_data[i][0]
+	y = all_data[i][1]
+	z = all_data[i][2]
+	j = 3
+	while (j < NUM_COORD):
+		all_data[i][j] -= x
+		all_data[i][j+1] -= y
+		all_data[i][j+2] -= z
+		j += 3
+	all_data[i][0] = all_data[i][1] = all_data[i][2] = 0
+
+td_data = np.zeros(shape=(SIZE, 4, 3))
+cav_labels = np.zeros(shape=(SIZE))
+for i in range(SIZE):
+	j = 3
+	while (j < NUM_COORD):
+		td_data[i][int(j/3)-1][0] = all_data[i][j]
+		td_data[i][int(j/3)-1][1] = all_data[i][j+1]
+		td_data[i][int(j/3)-1][2] = all_data[i][j+2]
+		j += 3
+	dist = 10
+	for k in range(4):
+		dx = abs(td_data[i][k][0])
+		if dx > 2.5:
+			dx -= 5
+		dy = abs(td_data[i][k][1])
+		if dy > 2.5:
+			dy -= 5
+		dz = abs(td_data[i][k][2])
+		if dz > 2.5:
+			dz -= 5
+		temp = math.sqrt(dx**2 + dy**2 + dz**2)
+		if temp < dist:
+			dist = temp
+	cav_labels[i] = dist
+	if dist > 1:
+		print(dist, td_data[i])
+
+
+FORT = np.zeros(shape=(int(2/bin)))
+FORT_cav = np.zeros(shape=(int(0.5/bin)))
+for i in cav_labels:
+	FORT_cav[int(math.floor(i/bin))] += 1
+with open("tetra_skip.dat") as a:
+	for line in a:
+		l = line.split('   ')
+		final = []
+		for i in range(len(l)):
+			l[i].strip()
+			if (l[i] == '' or len(l[i]) == 2): continue
+			try:
+				final.append(float(l[i]))
+			except:
+				print(len(l[i]))		
+		#print(final)
+		try:
+			FORT[int(math.floor((final[0]+1)/bin))] = final[1]
+		except:
+			print("ERROR IN ADDING TO LIST:", final[0])
+
+model = load_model("multi_head.h5")
+pre = model.predict(td_data)
+results = model.evaluate(td_data, [cav_labels, all_labels])
+#print(pre[0], len(pre))
+for i in range(len(pre[1])):
+	j = math.floor((pre[1][i]+1)/bin)
+	if (j >= 200):
+		#print("HERE", j)
+		#continue		
+		j = 199
+	ML[int(j)] += 1
+
+for i in range(len(ML)):
+	ML[i] /= 4142.
+
+for i in range(len(pre[0])):
+	j = int(math.floor((pre[0][i])/bin))
+	try:
+		ML_cav[j] += 1
+	except:
+		pass
+		#print("error", j)
+
+
+for i in range(len(ML_cav)):
+	FORT_cav[i] /= 4142
+	FORT_cav[i] /= 5
+	#FORT_cav[i] *= 25
+	ML_cav[i] /= 4142
+	ML_cav[i] /= 5
+	#ML_cav[i] *= 25
+print(len(cav_labels), len(pre[0]))
+
+a = plt.figure(1)
+plt.title("Traditional vs. Machine Learned TOP")
+plt.plot(x_graph, FORT, 'olive', label='Actual')
+plt.plot(x_graph, ML, 'red', label='ML')
+plt.legend(loc='upper left')
+plt.xlabel('q')
+plt.ylabel('Prob')
+
+x_graph_cav = np.zeros(shape=(int(0.5/bin)))
+
+for i in range(len(x_graph_cav)):
+	x_graph_cav[i] = i*bin+0.5*bin
+
+
+d = plt.figure(4)
+plt.title("Traditional vs. Machine Learned Cavity Formation")
+plt.plot(x_graph_cav, FORT_cav, 'olive', label='Actual')
+plt.plot(x_graph_cav, ML_cav, 'red', label='ML')
+plt.legend(loc='upper right')
+plt.xlabel('Radius (nm)')
+plt.ylabel('Prob')
+
+
+
+'''
+heat_pre = model.predict(heat_data)
+# heat map actual
+b = plt.figure(2)
+plt.scatter(heat_y, heat_z, s=100, c=ss_label, cmap="hot", vmin=0, vmax=1, alpha=0.9)
+plt.colorbar()
+plt.xlabel("y axis")
+plt.ylabel("z axis")
+plt.title("Actual")
+
+
+# heat map ML
+c = plt.figure(3)
+plt.scatter(heat_y, heat_z, s=100, c=heat_pre, cmap="hot", vmin=0, vmax=1, alpha=0.9)
+plt.colorbar()
+plt.xlabel("y axis")
+plt.ylabel("z axis")
+plt.title("ML")
+
+'''
+a.show()
+'''
+b.show()
+c.show()
+'''
+d.show()
+plt.show()
+
+
+
+
